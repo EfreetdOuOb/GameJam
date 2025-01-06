d5030417d1ed1d95a5c77b139405a0673f7a6da8
EfreetdOuOb <efreetdouob@gmail.com>
2025-01-06 21:24:19 +0800

    人物移動，鏡頭設置、

--- Game-Jam-Project/Assets/Scripts/PlayerController.cs
+++ Game-Jam-Project/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,111 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static BaseState;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    [Header("Player")]
+    [SerializeField] public float moveSpeed;
+    [SerializeField] public float jumpForce;
+    [SerializeField] private LayerMask groundLayer;
+
+    private Rigidbody2D rb2d;
+    private Animator animator;
+    private BaseState currentState;
+    private bool isGrounded ;
+    private float jumpTime;
+
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
     void Start()
     {
-        
+        SetCurrentState(new Idle(this)); 
     }
-
-    // Update is called once per frame
+     
     void Update()
     {
-        
+        currentState.Update();
+        Face();
+
+        if (Input.GetButtonDown("Jump") && isGrounded )
+        {
+            Jump();
+        }
     }
+
+    void FixedUpdate()
+    {
+        currentState.FixedUpdate();
+        if (isGrounded == false)
+        {
+            jumpTime += Time.fixedDeltaTime;
+            if (jumpTime > 0.1f && rb2d.Cast(Vector2.down, new RaycastHit2D[10], 0.1f) > 0)
+            {
+                isGrounded= true;
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        currentState.OnTriggerEnter2D(collision);
+    }
+     
+    public bool PressArrowKey()
+    {
+        return Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0.1 ;
+    }
+
+    public void Move()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        rb2d.velocity = new Vector2(horizontalInput * moveSpeed, rb2d.velocity.y);
+    }
+        
+    public void Stop()
+    {
+        rb2d.velocity = new Vector2(0, rb2d.velocity.y);
+    } 
+     
+    
+     
+    public void Face()
+    {
+        bool flipped = GetComponentInChildren<SpriteRenderer>().flipX;
+        if (Input.GetAxis("Horizontal") < 0 && !flipped)
+        {
+            GetComponentInChildren<SpriteRenderer>().flipX = true;
+        }
+        if (Input.GetAxis("Horizontal") > 0 && flipped)
+        {
+            GetComponentInChildren<SpriteRenderer>().flipX = false;
+        } 
+    }
+
+    public void PlayAnimation(string clip)
+    {
+        animator.Play(clip);
+    }
+
+    public bool IsAnimationDone(string _aniName)
+    {
+        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        return (stateInfo.IsName(_aniName) && stateInfo.normalizedTime >= 1.0);
+    }
+     
+    public void SetCurrentState(BaseState state)
+    {
+        currentState = state;
+    }
+    private void Jump()
+    {
+        isGrounded = false;
+        jumpTime = 0;
+        rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce); 
+    }
+     
 }
